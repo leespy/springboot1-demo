e5c8a5e22141daadbc888fdf426325fa339dade4
lijinlong02 <lijinlong02@baidu.com>
2018-11-26 22:50:37 +0800

    first commit

---
+++ src/main/java/com/leespy/springboot1demo/Springboot1DemoApplication.java
@@ -0,0 +1,12 @@
+package com.leespy.springboot1demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot1DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot1DemoApplication.class, args);
+    }
+}
